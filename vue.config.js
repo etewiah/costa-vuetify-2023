e729db7f93ef70e72308eb6ceb594376dc7aadb6
Ed Tewiah <etewiah@hotmail.com>
2023-10-21 21:17:16 +0100

    vue add i18n

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableBridge: false
+    }
+  }
 }
