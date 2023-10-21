3398e86f1e49b465fa749719ba484f0170b57196
Ed Tewiah <etewiah@hotmail.com>
2023-10-21 23:22:46 +0100

    Add css and fonts

--- src/main.js
+++ src/main.js
@@ -6,6 +6,19 @@ import store from './store'
 import router from './router'
 import i18n from './i18n'
 
+
+// import 'vuetify/src/stylus/app.styl'
+// july 2019 - below replaced with
+// import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import '@/assets/css/marbella.css'
+import '@/assets/css/hussein_extra.css'
+// https://vuetifyjs.com/en/framework/icons#installing-fonts
+// import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+// PageSpeed advices removing above as its top recommendation for improving performance
+// Consider using <link rel=preload> to prioritize fetching resources that are currently requested 
+
+
+
 Vue.config.productionTip = false
 
 import FastI18n from '@/plugins/FastI18n'
